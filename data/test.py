27c939a5e828aea0a7965ad916d05abb0cde5ed7
github-johndoe <spam@spataru.at>
2015-04-27 21:41:44 +0100

    work

---
+++ data/test.py
@@ -0,0 +1,45 @@
+import sys
+from PyQt4 import QtCore, QtGui
+
+from basic_ui import Ui_Dialog
+from logo import *
+
+
+class MyForm(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        QtCore.QObject.connect(self.ui.pushButton, QtCore.SIGNAL("clicked()"), self.setName)
+
+    def setName(self):
+        a = self.ui.lineEdit.text()
+        dirColors = FileFinder("./palettes", "xml")
+        dirShapes = FileFinder("./primitives", "svg")
+        dirSymbols = FileFinder("./symbols", "svg")
+        dirTemplates = FileFinder("./templates", "xml")
+        dirFonts = FileFinder("./fonts", "xml")
+
+        colorPalette1 = ColorPalette(dirColors.getRandomFile())
+        shape01 = ShapePrimitive(dirShapes.getRandomFile())
+        symbol01 = ShapeSymbol(dirSymbols.getRandomFile())
+        font01 = ShapeText(dirFonts.getRandomFile())
+
+        b = str(a)
+
+        font01.text = b
+
+        logo = LogoTemplate(dirTemplates.getRandomFile(), shape01, symbol01, font01, colorPalette1)
+        logo.writeFile("logo.svg")
+        scene = QtGui.QGraphicsScene()
+
+        scene.addPixmap(QtGui.QPixmap('logo.svg'))
+        self.ui.graphicsView.setScene(scene)
+        self.ui.graphicsView.show()
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    myapp = MyForm()
+    myapp.show()
+    sys.exit(app.exec_())
